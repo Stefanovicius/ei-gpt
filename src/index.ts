8ebf84a2a1d797ede9ac536623eed97ce03ca340
adomas_stefanovicius <dev.adomas@gmail.com>
2024-04-25 15:08:49 +0300

    chore: made the prompt optional, to be able to set the api key separately, some minor tweaks

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,22 @@ import { getApiKey } from './credentials'
 import { GPT } from './gpt'
 
 program
-  .version('0.1.1')
-  .description('A CLI for chatting with ChatGPT.')
-  .arguments('<prompt>')
+  .name('ei')
+  .version('0.1.2')
+  .description(
+    `"Ei, GPT!" is a user-friendly Command Line Interface (CLI) tool that allows you to interact with OpenAI's ChatGPT directly from your terminal.`,
+  )
+
+program
+  .arguments('[prompt]')
   .option('-k, --set-api-key', 'Set API key interactively')
   .action(async (prompt, cmd) => {
     const apiKey = await getApiKey(cmd.setApiKey ?? false)
+    if (!prompt) return
     const gpt = new GPT(apiKey)
     await gpt.sendMessage(prompt)
   })
 
 program.parse(process.argv)
+
+if (!process.argv.slice(2).length) program.help()
